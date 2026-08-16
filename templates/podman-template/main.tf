03e89382909168c88fca1e23d904b1add29c6a5e
Javier Arrieta <javier@techdelivery.es>
2026-08-16 20:46:17 +0100

    fix(podman-template): point workspace_image to dev-tooling + editor build 7e8241e

--- templates/podman-template/main.tf
+++ templates/podman-template/main.tf
@@ -131,7 +131,7 @@ data "coder_parameter" "workspace_image" {
   display_name = "Workspace image"
   description  = "Workspace container image (registry/repo:tag)"
   type         = "string"
-  default      = "registry.l.arrieta.eu/coder-workspace:ef3e965"
+  default      = "registry.l.arrieta.eu/coder-workspace:7e8241e"
   mutable      = true
 }
 
