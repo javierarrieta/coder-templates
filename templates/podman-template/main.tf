8d926e71804e3a19f7523047f71e266cb51e6bd3
javierarrieta <javier.arrieta@gmail.com>
2026-08-21 15:13:38 +0000

    fix: remove spaces around CODER_AGENT_TOKEN env var
    terraform fmt in 5bc4d00 added spaces around '=', producing
    'CODER_AGENT_TOKEN = <token>'. Docker parses this as key
    'CODER_AGENT_TOKEN ' (trailing space) which the agent doesn't read.
    Restore 'CODER_AGENT_TOKEN=<token>' format.

--- templates/podman-template/main.tf
+++ templates/podman-template/main.tf
@@ -170,7 +170,7 @@ resource "docker_container" "workspace" {
   userns_mode = "keep-id:uid=1000,gid=1000"
 
   env = [
-    "CODER_AGENT_TOKEN = ${coder_agent.main.token}",
+    "CODER_AGENT_TOKEN=${coder_agent.main.token}",
   ]
 
   command = ["sh", "-c", coder_agent.main.init_script]
